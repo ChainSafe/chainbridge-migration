e6335a60dc5401e87c94e38712a6282b74b28229
Mak Muftic <mak@chainsafe.io>
2022-02-02 14:13:01 +0100

    Add function for pausing bridge contract

--- scripts/pause-bridge.go
+++ scripts/pause-bridge.go
@@ -3,10 +3,14 @@ package scripts
 import (
 	"bridge-scripts/util"
 	"context"
+	"crypto/ecdsa"
+	"errors"
 	"fmt"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"math/big"
 	"strconv"
@@ -15,7 +19,6 @@ import (
 )
 
 func PauseBridge(v1BridgeConfig *util.V1BridgeConfig, config *util.Config) error {
-
 	var hasChainPendingProposals = map[string]bool{}
 	for _, c := range v1BridgeConfig.Chains {
 		hasChainPendingProposals[c.Id] = true
@@ -25,9 +28,6 @@ func PauseBridge(v1BridgeConfig *util.V1BridgeConfig, config *util.Config) error
 		for _, chain := range v1BridgeConfig.Chains {
 			fmt.Printf("Checking for pending proposals on chain %s ...\n", chain.Name)
 			client, err := ethclient.Dial(chain.Endpoint)
-			if err != nil {
-				return err
-			}
 
 			startingBlock := config.StartingBlocks[chain.Id]
 			if startingBlock == "" {
@@ -71,20 +71,18 @@ func PauseBridge(v1BridgeConfig *util.V1BridgeConfig, config *util.Config) error
 		for _, chain := range v1BridgeConfig.Chains {
 			pk := config.PrivateKeys[chain.Id]
 			if pk == "" {
-				fmt.Printf("Unable to pause bridge contract, missing private key for chain: %s\n", chain.Name)
+				fmt.Printf("Unable to pause bridge contract, missing private key for chain %s\n", chain.Name)
 			}
-			err := pauseBridgeContract(chain, pk)
+			txHash, err := pauseBridgeContract(chain, pk)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Printf("Unable to pause bridge contract for chain %s, because: %v\n", chain.Name, err)
+			} else {
+				fmt.Printf("Transaction for pausing bridge contract on chain %s submitted with hash %s\n",
+					chain.Name, txHash.Hex())
 			}
 		}
 	}
 
-	return nil
-}
-
-func pauseBridgeContract(chain util.RawChainConfig, pk string) error {
-	fmt.Printf("Paused bridge contract on chain %s", chain.Name)
 	return nil
 }
 
@@ -182,4 +180,82 @@ func getAllPendingProposals(
 	}
 
 	return pendingProposals, err
+}
+
+func pauseBridgeContract(chain util.RawChainConfig, pk string) (*common.Hash, error) {
+	bAbi, err := abi.JSON(strings.NewReader(util.BridgeABI))
+	if err != nil {
+		return nil, err
+	}
+	txData, err := bAbi.Pack("adminUnpauseTransfers")
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := ethclient.Dial(chain.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	privateKey, err := crypto.HexToECDSA(pk)
+	if err != nil {
+		return nil, err
+	}
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, errors.New("error casting public key to ECDSA")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	gasFeeCap, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	gasTipCap, err := client.SuggestGasTipCap(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	bridgeAddress := chain.Opts["bridge"]
+	if bridgeAddress == "" {
+		return nil, errors.New("bridge address not defined")
+	}
+	toAddress := common.HexToAddress(bridgeAddress)
+
+	amount := big.NewInt(0)
+	gasLimit := uint64(2100000)
+
+	tx := types.NewTx(&types.DynamicFeeTx{
+		Nonce:     nonce,
+		To:        &toAddress,
+		GasFeeCap: gasFeeCap,
+		GasTipCap: gasTipCap,
+		Gas:       gasLimit,
+		Value:     amount,
+		Data:      txData,
+	})
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewLondonSigner(chainID), privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return nil, err
+	}
+
+	hash := signedTx.Hash()
+	return &hash, nil
 }
